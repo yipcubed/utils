cee30e38a1958d1365e9250bbdc6793b89729326
Yusuf Pisan <pisan@uw.edu>
2020-11-13 16:33:01 -0800

    me

--- PrintUtils.hpp
+++ PrintUtils.hpp
@@ -11,20 +11,43 @@
 
 using namespace std;
 
-template<typename T>
-void printvar(const string &str, const T &t) {
-    cout << str << " = " << t << " ";
+template <typename T> void printvar(const string &str, const T &t) {
+  cout << str << " = " << t << " ";
 }
 
-#define PV(x) do { printvar(#x, (x)); } while (false)
+#define PV(x)                                                                  \
+  do {                                                                         \
+    printvar(#x, (x));                                                         \
+  } while (false)
 
-#define PV1(x) do { PV(x); cout << endl; } while (false)
+#define PV1(x)                                                                 \
+  do {                                                                         \
+    PV(x);                                                                     \
+    cout << endl;                                                              \
+  } while (false)
 
-#define PV2(x, y) do { PV(x); PV(y); cout << endl; } while (false)
+#define PV2(x, y)                                                              \
+  do {                                                                         \
+    PV(x);                                                                     \
+    PV(y);                                                                     \
+    cout << endl;                                                              \
+  } while (false)
 
-#define PV3(x, y, z) do { PV(x); PV(y); PV(z); cout << endl; } while (false)
+#define PV3(x, y, z)                                                           \
+  do {                                                                         \
+    PV(x);                                                                     \
+    PV(y);                                                                     \
+    PV(z);                                                                     \
+    cout << endl;                                                              \
+  } while (false)
 
-#define PV4(x, y, z, w) do { PV(x); PV(y); PV(z); PV(w); cout << endl; } while (false)
+#define PV4(x, y, z, w)                                                        \
+  do {                                                                         \
+    PV(x);                                                                     \
+    PV(y);                                                                     \
+    PV(z);                                                                     \
+    PV(w);                                                                     \
+    cout << endl;                                                              \
+  } while (false)
 
-
-#endif //SAMPLE_PRINTUTILS_H
+#endif // SAMPLE_PRINTUTILS_H
